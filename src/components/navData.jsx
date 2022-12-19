1ca1cc5a13aa39556618d348c44a24e3d554875b
mahdi_rezaei <mahdir798@gmail.com>
2022-12-19 20:56:20 +0330

    init nav

---
+++ src/components/navData.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export const links = [
+     {
+          id: 1,
+          url: '/',
+          text: 'خانه',
+     },
+       {
+          id: 2,
+          url: '/',
+          text: 'درباره ما',
+     },
+       {
+          id: 3,
+          url: '/',
+          text: 'تماس با ما',
+     },
+       {
+          id: 4,
+          url: '/',
+          text: 'پروژه ها',
+     }
+     
+]
+
+export const socials = [
+     {
+          id: 1,
+          url: 'https://twitter.com',
+          icon: <i class="bi bi-linkedin"></i>
+     },
+     {
+          id: 2,
+          url: 'https://twitter.com',
+          icon: <i class="bi bi-telegram"></i>
+     },
+     {
+          id: 3,
+          url: 'https://twitter.com',
+          icon: <i class="bi bi-facebook"></i>
+     },
+     {
+          id: 4,
+          url: 'https://twitter.com',
+          icon: <i class="bi bi-twitter"></i>
+     },
+] 
